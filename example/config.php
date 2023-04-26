949eac8016e4421091fbd37b78053aca5f574808
1234ru <1234ru@gmail.com>
2023-04-26 21:31:29 +0300

    blank page added

--- example/config.php
+++ example/config.php
@@ -1,13 +1,35 @@
 <?php return 
 [
-    'websun' => __DIR__ . '/../_lib/websun.php', // путь к файлу https://webew.ru/articles/3609.webew
+    // 'template_parser' => __DIR__ . '/../_lib/websun.php', // путь к файлу https://webew.ru/articles/3609.webew
 
 	'template' => 'templates/_main.tpl',
-	
-	'js' => '/js/jquery.min.js',
+
+    'html_generation_code' => function($args) {
+        $html = websun_parse_template_path(
+            $args['page'],
+            $args['$template'],
+            $args['templates_dir']
+        );
+        return $html;
+    },
+
+    // 'base_href' => '', // будет определено автоматически
+
+    'css' => [],
+
+	'js' => [
+        'js/jquery.min.js'
+    ],
 
 	'pages' => [
-		1 => [ // с явно указанным индексом - чтобы нумерация начиналась с единицы
+        // Нумерация начинается с 0, что правильно - это просто общие стили
+        [
+            'title' => 'Шапка + футер + общие стили',
+            'dir' => '_blank',
+            'content' => 'content.tpl',
+            'variants' => [],
+        ],
+		[
 			'title' => 'Главная страница',
 			'dir' => '_mainpage',
 			'content' => 'content.tpl',
